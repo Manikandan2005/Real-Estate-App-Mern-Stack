44fcf4530bbbee65a60c17583d2e5ace441a708e
Manikandan2005 <manismvr321@gmail.com>
2024-07-05 23:39:25 +0530

    Final Changes

--- api/app.js
+++ api/app.js
@@ -1,31 +1,26 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import postRoutes from "./routes/Post.route.js";
-import authRoutes from "./routes/Auth.route.js";
-import testRoutes from "./routes/Test.route.js";
-import userRoutes from "./routes/User.route.js";
-import ChatRoutes from "./routes/Chat.route.js";
-import messageRoute from "./routes/Message.route.js";
-import dotenv from "dotenv";
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const postRoutes = require('./routes/Post.route.js');
+const authRoutes = require('./routes/Auth.route.js');
+const testRoutes = require('./routes/Test.route.js');
+const userRoutes = require('./routes/User.route.js');
+const ChatRoutes = require('./routes/Chat.route.js');
+const messageRoute = require('./routes/Message.route.js');
+const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-const corsOptions = {
-  origin: process.env.CLIENTURL,
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: process.env.CLIENTURL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use("/posts", postRoutes);
-app.use("/auth", authRoutes);
-app.use("/test", testRoutes);
-app.use("/users", userRoutes);
-app.use("/chats", ChatRoutes);
-app.use("/messages", messageRoute);
+app.use('/posts', postRoutes);
+app.use('/auth', authRoutes);
+app.use('/test', testRoutes);
+app.use('/users', userRoutes);
+app.use('/chats', ChatRoutes);
+app.use('/messages', messageRoute);
 
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
