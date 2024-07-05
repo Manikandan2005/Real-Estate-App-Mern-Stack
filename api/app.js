1865962ba3ee6cab2d9c36c2ac13b99aea70c1f9
Manikandan2005 <manismvr321@gmail.com>
2024-07-05 23:50:56 +0530

    Updated app.js

--- api/app.js
+++ api/app.js
@@ -1,26 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const postRoutes = require('./routes/Post.route.js');
-const authRoutes = require('./routes/Auth.route.js');
-const testRoutes = require('./routes/Test.route.js');
-const userRoutes = require('./routes/User.route.js');
-const ChatRoutes = require('./routes/Chat.route.js');
-const messageRoute = require('./routes/Message.route.js');
-const dotenv = require('dotenv');
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import postRoutes from './routes/Post.route.js';
+import authRoutes from './routes/Auth.route.js';
+import testRoutes from './routes/Test.route.js';
+import userRoutes from './routes/User.route.js';
+import chatRoutes from './routes/Chat.route.js';
+import messageRoute from './routes/Message.route.js';
 
+// Load environment variables from .env file
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Middleware
 app.use(cors({ origin: process.env.CLIENTURL, credentials: true }));
+app.use(helmet()); // Security headers
 app.use(cookieParser());
 app.use(express.json());
+app.use(morgan('dev')); // Logger
+
+// Routes
 app.use('/posts', postRoutes);
 app.use('/auth', authRoutes);
 app.use('/test', testRoutes);
 app.use('/users', userRoutes);
-app.use('/chats', ChatRoutes);
+app.use('/chats', chatRoutes);
 app.use('/messages', messageRoute);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start server
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
