c4ad716634b8e60da756616a7cebb41277489aec
Manikandan2005 <manismvr321@gmail.com>
2024-07-07 18:28:14 +0530

    Deployment

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-app-mern-stack-ppcwnyqng-manikandans-projects.vercel.app",
+  baseURL: "real-estate-app-mern-stack-dlnp9hazw-manikandans-projects.vercel.app",
   withCredentials: true,
 });
 
