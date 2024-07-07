f8428addda7ec286baf6018b603c9ae7224a273a
Manikandan2005 <manismvr321@gmail.com>
2024-07-07 19:15:12 +0530

    Depy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-app-mern-stack.vercel.app",
+  baseURL: "https://real-estate-app-mern-stack-98jf.vercel.app",
   withCredentials: true,
 });
 
